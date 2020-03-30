c77cc8c3c9b411d67a7d58863a1fe33d36a980d5
Roy Lazy <lazyroy@gmail.com>
2020-03-30 09:39:28 +0300

    add server port selection, small fixes

--- client.c
+++ client.c
@@ -9,7 +9,7 @@
 #include "sock-portable.h"
 
 enum {
-	BUFSIZE = 4096,
+    BUFSIZE = 4096,
 };
 
 char *program_name;
@@ -17,57 +17,56 @@ char *program_name;
 
 void print_usage_exit(void)
 {
-	fprintf(stderr, "usage: %s SERVER_CID:PORT\n", program_name);
-	exit(2);
+    fprintf(stderr, "usage: %s <SERVER_CID:PORT>\n", program_name);
+    exit(2);
 }
 
 int main(int argc, char **argv)
 {
-	struct sockaddr_vm their_addr = {0};
-	int vmci_address_family;
-	unsigned int server_cid, server_port;
-	int sockfd = -1;
-	char *p;
+    struct sockaddr_vm their_addr = {0};
+    int vmci_address_family;
+    unsigned int server_cid, server_port;
+    int sockfd = -1;
+    char *p;
 
-	program_name = basename(argv[0]);
+    program_name = basename(argv[0]);
 
-	if (argc != 2)
-		print_usage_exit();
+    if (argc != 2)
+        print_usage_exit();
 
-	if (!(p = strchr(argv[1], ':'))) {
-		fprintf(stderr, "%s: invalid SERVER_CID:PORT: \"%s\"\n", program_name, argv[1]);
-		exit(2);
-	}
+    if (!(p = strchr(argv[1], ':'))) {
+        fprintf(stderr, "%s: invalid SERVER_CID:PORT: \"%s\"\n", program_name, argv[1]);
+        exit(2);
+    }
 
-	*p = '\0';
-	server_cid = (unsigned int)strtoul(argv[1], &p, 0);
-	assert(*p == '\0');
-	server_port = (unsigned int)strtoul(p+1, &p, 0);
-	assert(*p == '\0');
+    server_cid = (unsigned int)strtoul(argv[1], &p, 0);
+    assert(*p == ':');
+    server_port = (unsigned int)strtoul(p+1, &p, 0);
+    assert(*p == '\0');
 
-	socket_startup();
+    socket_startup();
 
-	if ((vmci_address_family = VMCISock_GetAFValue()) < 0) {
-		fprintf(stderr, "VMCISock_GetAFValue failed: %d. You probably need root privileges\n", vmci_address_family);
-		goto cleanup;
-	}
-	if ((sockfd = socket(vmci_address_family, SOCK_STREAM, 0)) == -1) {
-		perror("socket");
-		goto cleanup;
-	}
+    if ((vmci_address_family = VMCISock_GetAFValue()) < 0) {
+        fprintf(stderr, "VMCISock_GetAFValue failed: %d. You probably need root privileges\n", vmci_address_family);
+        goto cleanup;
+    }
+    if ((sockfd = socket(vmci_address_family, SOCK_STREAM, 0)) == -1) {
+        perror("socket");
+        goto cleanup;
+    }
 
-	their_addr.svm_family = vmci_address_family;
-	their_addr.svm_cid = server_cid; // in native byte order, i.e. little endian for the platforms that VMWare runs on
-	their_addr.svm_port = server_port;
-	fprintf(stderr, "connecting to %u:%u\n", server_cid, server_port);
-	if (connect(sockfd, (struct sockaddr *) &their_addr, sizeof(their_addr)) == -1) {
-		perror("connect");
-		goto close;
-	}
-	send(sockfd, "hello", 5, 0);
+    their_addr.svm_family = vmci_address_family;
+    their_addr.svm_cid = server_cid; // in native byte order, i.e. little endian for the platforms that VMWare runs on
+    their_addr.svm_port = server_port;
+    fprintf(stderr, "connecting to %u:%u\n", server_cid, server_port);
+    if (connect(sockfd, (struct sockaddr *) &their_addr, sizeof(their_addr)) == -1) {
+        perror("connect");
+        goto close;
+    }
+    send(sockfd, "hello\n", 6, 0);
 close:
-	if (sockfd > 0)
-		socket_close(sockfd);
+    if (sockfd > 0)
+        socket_close(sockfd);
 cleanup:
-	return 0;
+    return 0;
 }
--- server.c
+++ server.c
@@ -9,117 +9,142 @@
 
 #include "sock-portable.h"
 
+char *program_name;
+
+void print_usage_exit(void)
+{
+    fprintf(stderr, "usage: %s <PORT|0>\n", program_name);
+    fprintf(stderr, "\t0 - port auto selection\n");
+    exit(2);
+}
+
 enum {
-	BUFSIZE = 4096,
-	CONNECTION_BACKLOG = 10,
+    BUFSIZE = 4096,
+    CONNECTION_BACKLOG = 10,
 };
 
-int main()
+int main(int argc, char **argv)
 {
-	int listen_fd, client_fd, nfds;
-	uint64_t buf_size, t;
-	socklen_t size;
-	struct sockaddr_vm my_addr = {0}, their_addr;
-	int vmci_address_family;
-	fd_set read_fds;
-	unsigned int cid;
-	uint8_t buf[BUFSIZE];
+    unsigned int server_cid, server_port;
+    int listen_fd, client_fd, nfds;
+    uint64_t buf_size, t;
+    socklen_t size;
+    struct sockaddr_vm my_addr = {0}, their_addr;
+    int vmci_address_family;
+    fd_set read_fds;
+    uint8_t buf[BUFSIZE];
+    char *p;
 
-	socket_startup();
+    program_name = basename(argv[0]);
 
-	if ((vmci_address_family = VMCISock_GetAFValue()) < 0) {
-		fprintf(stderr, "VMCISock_GetAFValue failed: %d. You probably need root privileges\n", vmci_address_family);
-		goto cleanup;
-	}
+    if (argc != 2)
+        print_usage_exit();
 
-	if ((listen_fd = socket(vmci_address_family, SOCK_STREAM, 0)) == -1) {
-		perror("socket");
-		goto cleanup;
-	}
+    server_port = (unsigned int)strtoul(argv[1], &p, 0);
+    assert(*p == '\0');
 
-	/*
-	 * SO_VMCI_BUFFER_SIZE – Default size of communicating buffers; 65536 bytes if not set.
-	 * SO_VMCI_BUFFER_MIN_SIZE – Minimum size of communicating buffers; defaults to 128 bytes.
-	 * SO_VMCI_BUFFER_MAX_SIZE – Maximum size of communicating buffers; defaults to 262144 bytes.
-	 */
+    if (server_port == 0) 
+         server_port = VMADDR_PORT_ANY;
 
-	buf_size = 32768;
-	/* reduce buffer to above size and check */
-	if (setsockopt(listen_fd, vmci_address_family, SO_VMCI_BUFFER_SIZE, (void *)&buf_size, sizeof(buf_size)) == -1) {
-		perror("setsockopt");
-		goto close;
-	}
+    socket_startup();
 
-	size = sizeof(t);
-	if (getsockopt(listen_fd, vmci_address_family, SO_VMCI_BUFFER_SIZE, (void *)&t, &size) == -1) {
-		perror("getsockopt");
-		goto close;
-	}
-	if (t != buf_size) {
-		fprintf(stderr, "SO_VMCI_BUFFER_SIZE not set to size requested.\n");
-		goto close;
-	}
+    if ((vmci_address_family = VMCISock_GetAFValue()) < 0) {
+        fprintf(stderr, "VMCISock_GetAFValue failed: %d. You probably need root privileges\n", vmci_address_family);
+        goto cleanup;
+    }
 
-	my_addr.svm_family = vmci_address_family;
-	my_addr.svm_cid = VMADDR_CID_ANY;
-	my_addr.svm_port = VMADDR_PORT_ANY;
-	if (bind(listen_fd, (struct sockaddr *) &my_addr, sizeof(my_addr)) == -1) {
-		perror("bind");
-		goto close;
-	}
+    if ((listen_fd = socket(vmci_address_family, SOCK_STREAM, 0)) == -1) {
+        perror("socket");
+        goto cleanup;
+    }
 
-	if ((cid = VMCISock_GetLocalCID()) == (unsigned int)-1) {
-		fprintf(stderr, "VMCISock_GetLocalCID failed\n");
-	} else {
-		fprintf(stderr, "server cid: %u\n", cid);
-	}
+    /*
+     * SO_VMCI_BUFFER_SIZE – Default size of communicating buffers; 65536 bytes if not set.
+     * SO_VMCI_BUFFER_MIN_SIZE – Minimum size of communicating buffers; defaults to 128 bytes.
+     * SO_VMCI_BUFFER_MAX_SIZE – Maximum size of communicating buffers; defaults to 262144 bytes.
+     */
 
-	size = sizeof(my_addr);
-	if (getsockname(listen_fd, (struct sockaddr *)&my_addr, &size) == -1) {
-		perror("getsockname");
-		goto close;
-	}
+    buf_size = 32768;
+    /* reduce buffer to above size and check */
+    if (setsockopt(listen_fd, vmci_address_family, SO_VMCI_BUFFER_SIZE, (void *)&buf_size, sizeof(buf_size)) == -1) {
+        perror("setsockopt");
+        goto close;
+    }
 
-	fprintf(stderr, "server (cid, port): %u:%u\n", my_addr.svm_cid, my_addr.svm_port);
+    size = sizeof(t);
+    if (getsockopt(listen_fd, vmci_address_family, SO_VMCI_BUFFER_SIZE, (void *)&t, &size) == -1) {
+        perror("getsockopt");
+        goto close;
+    }
+    if (t != buf_size) {
+        fprintf(stderr, "SO_VMCI_BUFFER_SIZE not set to size requested.\n");
+        goto close;
+    }
 
-	for (;;) {
-		if (listen(listen_fd, CONNECTION_BACKLOG) == -1) {
-			perror("listen");
-			goto close;
-		}
+    my_addr.svm_family = vmci_address_family;
+    my_addr.svm_cid = VMADDR_CID_ANY;
+    my_addr.svm_port = server_port;
+    if (bind(listen_fd, (struct sockaddr *) &my_addr, sizeof(my_addr)) == -1) {
+        perror("bind");
+        goto close;
+    }
 
-		size = sizeof(their_addr);
-		if ((client_fd = accept(listen_fd, (struct sockaddr *) &their_addr, &size)) == -1) {
-			perror("accept");
-			goto close;
-		}
-		fprintf(stderr, "client connected\n");
+    if ((server_cid = VMCISock_GetLocalCID()) == (unsigned int)-1) {
+        fprintf(stderr, "VMCISock_GetLocalCID failed\n");
+    } else {
+        fprintf(stderr, "local cid: %u\n", server_cid);
+    }
 
-		FD_ZERO(&read_fds);
-		FD_SET(client_fd, &read_fds);
-		nfds = client_fd + 1;
-		if (select(nfds, &read_fds, NULL, NULL, NULL) == -1) {
-			perror("select");
-			goto close;
-		}
-		if (FD_ISSET(client_fd, &read_fds)) {
-			ssize_t s;
-			if ((s = recv(client_fd, (void*)buf, sizeof(buf), 0)) < 0) {
-				fprintf(stderr, "recv failed: %s\n", strerror(errno));
-			} else {
-				int i;
-				fprintf(stderr, "recved %lld bytes\n", (long long int)s);
-				for (i=0; i<s; i++) {
-					putc(buf[i], stderr);
-				}
-				putc('\n', stderr);
-			}
-		}
-		close(client_fd);
-	}
+    size = sizeof(my_addr);
+    if (getsockname(listen_fd, (struct sockaddr *)&my_addr, &size) == -1) {
+        perror("getsockname");
+        goto close;
+    }
+
+    fprintf(stderr, "listening on %u:%u\n", server_cid, my_addr.svm_port);
+
+// disable buffering
+//     setvbuf ( stdout , NULL , _IONBF , 1024 );
+
+    for (;;) {
+        if (listen(listen_fd, CONNECTION_BACKLOG) == -1) {
+            perror("listen");
+            goto close;
+        }
+
+        size = sizeof(their_addr);
+        if ((client_fd = accept(listen_fd, (struct sockaddr *) &their_addr, &size)) == -1) {
+            perror("accept");
+            goto close;
+        }
+        fprintf(stderr, "client connected\n");
+
+        FD_ZERO(&read_fds);
+        FD_SET(client_fd, &read_fds);
+        nfds = client_fd + 1;
+        if (select(nfds, &read_fds, NULL, NULL, NULL) == -1) {
+            perror("select");
+            goto close;
+        }
+        if (FD_ISSET(client_fd, &read_fds)) {
+            ssize_t s;
+            if ((s = recv(client_fd, (void*)buf, sizeof(buf), 0)) < 0) {
+                fprintf(stderr, "recv failed: %s\n", strerror(errno));
+            } else {
+                int i;
+                fprintf(stderr, "recved %lld bytes\n", (long long int)s);
+                for (i=0; i<s; i++) {
+                    putc(buf[i], stdout);
+                }
+//              putc('\n', stdout);
+                fflush(stdout);
+            }
+        }
+        close(client_fd);
+    }
 close:
-	socket_close(listen_fd);
+    socket_close(listen_fd);
 cleanup:
-	socket_cleanup();
-	return 0;
+    socket_cleanup();
+    return 0;
 }
